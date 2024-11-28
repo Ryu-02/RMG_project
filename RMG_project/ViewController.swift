8be4ddc1dc9a1dab583ce69574f5ea5ef08f8f9e
Ryu-02 <ymq02110192@gmail.com>
2024-11-28 15:05:06 +0900

    Initial Commit

---
+++ RMG_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RMG_project
+//
+//  Created by 유명규 on 11/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RMG_projectTests/RMG_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  RMG_projectTests.swift
+//  RMG_projectTests
+//
+//  Created by 유명규 on 11/28/24.
+//
+
+import Testing
+@testable import RMG_project
+
+struct RMG_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
